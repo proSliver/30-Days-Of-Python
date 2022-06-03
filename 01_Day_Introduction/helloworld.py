2fa0f2b49aae80f82727b15231f057920e753a95
proSliver <105857395+proSliver@users.noreply.github.com>
2022-06-03 20:00:59 +0800

    Update helloworld.py

--- 01_Day_Introduction/helloworld.py
+++ 01_Day_Introduction/helloworld.py
@@ -1,11 +1,11 @@
 # Introduction
 # Day 1 - 30DaysOfPython Challenge
 
-print(2 + 3)   # addition(+)
-print(3 - 1)   # subtraction(-)
-print(2 * 3)   # multiplication(*)
-print(3 / 2)   # division(/)
-print(3 ** 2)  # exponential(**)
+print(2 + 3)   # addition(+) 加法
+print(3 - 1)   # subtraction(-) 减法
+print(2 * 3)   # multiplication(*) 乘法
+print(3 / 2)   # division(/) 除法
+print(3 ** 2)  # exponential(**) 乘方
 print(3 % 2)   # modulus(%)
 print(3 // 2)  # Floor division operator(//)
 
@@ -14,7 +14,7 @@ print(3 // 2)  # Floor division operator(//)
 print(type(10))                  # Int
 print(type(3.14))                # Float
 print(type(1 + 3j))              # Complex
-print(type('Asabeneh'))          # String
+print(type('Asabeneh'))          # String 字符串
 print(type([1, 2, 3]))           # List
 print(type({'name':'Asabeneh'})) # Dictionary
 print(type({9.8, 3.14, 2.7}))    # Set
