49c462b7242906b4250896770067b0f8183c18be
proSliver <105857395+proSliver@users.noreply.github.com>
2022-06-03 20:06:52 +0800

    Update helloworld.py

--- 01_Day_Introduction/helloworld.py
+++ 01_Day_Introduction/helloworld.py
@@ -5,7 +5,7 @@ print(2 + 3)   # addition(+)
 print(3 - 1)   # subtraction(-) 
 print(2 * 3)   # multiplication(*) 
 print(3 / 2)   # division(/) 
-print(3 ** 2)  # exponential(**) 
+print(3 ** 2)  # exponential(**) 乘方
 print(3 % 2)   # modulus(%)
 print(3 // 2)  # Floor division operator(//)
 
